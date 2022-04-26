c884ee82f7cfedb628abbfd72d804ee1243d64a1
wwwaw <aold88@gmail.com>
2022-04-26 23:30:05 +0300

    1

--- src/components/Words.js
+++ src/components/Words.js
@@ -14,7 +14,9 @@ export const generateWordSet = async () => {
     await fetch(wordBank)
         .then((response) => response.text())
         .then((result) => {
-            const wordArray = result.split('\r\n');
+            //const wordArray = result.split('\r\n');
+
+            const wordArray = result.split('\n');
             todaysWord = wordArray[Math.floor(Math.random() * wordArray.length)];
             wordSet = new Set(wordArray);
         });
